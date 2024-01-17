9b0cd32b17ef879df09492c96304e206dafa13a4
ayon121 <borshonsaha1234@gmail.com>
2024-01-17 21:25:53 +0600

    basic navbar section added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,20 +10,44 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  // links of the navbar
+  const navlinks = <>
+    <li><Link href="/" className='text-main font-bold hover:text-highlight_color'>Home</Link></li>
+    <li><Link href="/" className='text-main font-bold hover:text-highlight_color'>About Us</Link></li>
+    <li><Link href="/" className='text-main font-bold hover:text-highlight_color'>Contact</Link></li>
+  </>
+
   return (
-    <html lang="en">
+    <html >
       <body className={inter.className}>
         {/* navbar code.  It will be available on the whole website */}
-        <div>
-          {/* logo */}
-          <div>
-            <h1 className='text-main'>Logo 1</h1>
-          </div>
-          {/* navlinks */}
-          <div>
-              <h1>links</h1>
+        <div className=" bg-[#FFFFFF]">
+          <div className="navbar max-w-[2520px] mx-auto px-5">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </div>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  {navlinks}
+                </ul>
+              </div>
+              <a className="btn btn-ghost text-xl">Logo</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1 ">
+                {navlinks}
+              </ul>
+            </div>
+            <div className="navbar-end">
+              <a className="btn px-5 rounded-3xl bg-main hover:text-highlight_color">Login</a>
+            </div>
           </div>
         </div>
+
+        {/* navbar ended */}
+
+        
         {children}
       </body>
     </html>
