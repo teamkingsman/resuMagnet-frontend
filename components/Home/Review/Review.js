c50a68a387f0441e1f499abc4ba40c0a175503fc
heeraraiyan <heeraraiyan07@yahoo.com>
2024-01-18 01:41:58 +0600

    review section design done with out making slider

--- components/Home/Review/Review.js
+++ components/Home/Review/Review.js
@@ -1,17 +1,29 @@
 
-
+import { FaRegStar } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
 
 
 
 const Review = ({item}) => {
     const {id,name,designation,comment,rating} = item;
+
+    const renderStars = () => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            const StarIcon = i <= rating ? FaStar : FaRegStar;
+            stars.push(<StarIcon key={i} className="star-icon" />);
+        }
+        return stars;
+    };
     return (
-        <div className="text-center bg-neutral-400 px-10 py-4">
-            <h1 className="text-4xl font-bold text-main mb-2">{name}</h1>
-            <h3 className="text-xl mb-4">{designation}</h3>
-            <p className="font-bold ">{comment}</p>
-            <h3 className="text-2xl text-highlight_color mt-2">{rating}</h3>
+        <div className="text-center bg-sub_color px-20 py-10">
+        <h1 className="text-4xl font-bold text-main mb-2">{name}</h1>
+        <h3 className="text-2xl mb-4 font-bold italic text-neutral-700">{designation}</h3>
+        <p className="font-bold text-main text-xl">{comment}</p>
+        <div className="flex items-center justify-center mt-2 text-highlight_color text-2xl">
+            {renderStars()}
         </div>
+    </div>
 );
 };
 
--- components/Home/Review/Reviews.js
+++ components/Home/Review/Reviews.js
@@ -4,9 +4,13 @@ import reviewsData from '/assets/review.json'
 const Reviews = () => {
     return (
         <div>
-            <h1>Reviews</h1>
+            <div className='max-w-7xl mx-auto my-8'>
+            <p className='text-6xl font-bold text-main'>Loved</p>
+            <p className='text-5xl font-bold text-sub_color pl-52'>&</p>
+            <p className='text-6xl font-bold text-main pl-64'> Trusted <span className='text-xl text-neutral-600'>by. . . . . . .</span></p>
 
-            <div className="max-w-7xl mx-auto grid md:grid-cols-2 lg:grid-cols-2 gap-12 mb-20">
+            </div>
+            <div className="w-11/12 mx-auto grid md:grid-cols-2 lg:grid-cols-2 gap-12 mb-20">
                 {
 
                     reviewsData.map(item => <Review key={item.id} item={item}></Review>)
