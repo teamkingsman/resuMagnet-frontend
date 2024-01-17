309ee8d096ae55267860e3e83905f420fe092c59
heeraraiyan <heeraraiyan07@yahoo.com>
2024-01-17 19:40:41 +0600

    new

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+   
+  
+   
+
+    <h1 className='text-red-700'>heera</h1>
+
+    
+    </>
   )
 }
