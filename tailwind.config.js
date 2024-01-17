666e5cfe3f4d1c20bff303f6e06537e00f0825de
ayon121 <borshonsaha1234@gmail.com>
2024-01-17 14:11:08 +0600

    basic setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
