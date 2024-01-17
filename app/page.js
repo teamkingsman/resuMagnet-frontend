828f7c56c27d2ea215c68d1253a5414425299e97
heeraraiyan <heeraraiyan07@yahoo.com>
2024-01-17 23:57:19 +0600

    review section created

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
-import Banner from '@/components/Home/Banner/Banner'
-import Template from '@/components/Home/Template/Template'
-
+import Review from '@/components/Home/Review/Review'
 import Image from 'next/image'
+
 
 
 export default function Home() {
   return (
     <>
+    <Review></Review>
+   
+  
+   
 
-    <Banner/>
-    <Template></Template>
+   
 
-    </>
     
+    </>
   )
 }
---
+++ components/Home/Review/Review.js
@@ -0,0 +1,11 @@
+
+const Review = () => {
+    return (
+        <div>
+            <h1>review</h1>
+            
+        </div>
+    );
+};
+
+export default Review;
