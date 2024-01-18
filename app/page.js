20e3f6b0cf1089c067562b791b8b230611811bc5
Firoz Ahammad <firozahamed0@gmail.com>
2024-01-19 05:04:53 +0600

    Update dependencies and remove unused code

--- app/page.js
+++ app/page.js
@@ -2,8 +2,6 @@ import FeatureSection from "@/components/Home/Feature/FeatureSection";
 import Banner from '@/components/Home/Banner/Banner'
 import Template from '@/components/Home/Template/Template'
 import Reviews from '@/components/Home/Review/Reviews'
-// import Slider from '@/components/Home/Review/Slider'
-import Image from 'next/image'
 
 
 
---
+++ components/Home/Review/Slider.js
@@ -0,0 +1,29 @@
+
+// "use client";
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+
+
+
+// // import required modules
+// import { Pagination } from 'swiper/modules';
+// import Review from './Review';
+
+// const Slider = () => {
+//     return (
+//         <div>
+
+// <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+//         <SwiperSlide><Review></Review></SwiperSlide>
+        
+//       </Swiper>
+            
+//         </div>
+//     );
+// };
+
+// export default Slider;
