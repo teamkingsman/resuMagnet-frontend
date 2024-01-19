0803a58f2f96d8ab6a7ed4549134bb196785cb52
heeraraiyan <heeraraiyan07@yahoo.com>
2024-01-19 12:39:11 +0600

    background color changed for review section

--- components/Home/Review/Reviews.js
+++ components/Home/Review/Reviews.js
@@ -21,7 +21,7 @@ const Reviews = () => {
   };
 
   return (
-    <div className=''>
+    <div className='bg-neutral-100 my-20 pt-10'>
       <div className='flex md:justify-between items-center'>
         <div className='my-8 pl-10 md:pl-20'>
           <p className='text-6xl text-main'>Loved &</p>
