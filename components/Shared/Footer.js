116a7bd7fe5640cbfe64ea573d117f1435bd72c3
ayon121 <borshonsaha1234@gmail.com>
2024-01-19 16:00:05 +0600

    footer update

--- components/Shared/Footer.js
+++ components/Shared/Footer.js
@@ -3,6 +3,18 @@
 const Footer = () => {
     return (
         <footer className="footer p-10 bg-[#FFFFFF] text-base-content border-t-4 border-double border-highlight_color">
+             <form className="md:hidden block">
+                <header className="footer-title text-highlight_color">Newsletter</header>
+                <fieldset className="form-control w-full ">
+                    <label className="label">
+                        <span className="label-text text-main">Enter your email address</span>
+                    </label>
+                    <div className="join">
+                        <input type="text" placeholder="username@site.com" className="input input-bordered bg-[#FFFFFF] border-2 border-main join-item  w-full" />
+                        <button className="btn bg-main text-[#FFFFFF] join-item hover:text-highlight_color">Subscribe</button>
+                    </div>
+                </fieldset>
+            </form>
             <nav>
                 <header className="footer-title text-highlight_color ">Services</header>
                 <a className="link link-hover text-main">CV Making</a>
@@ -23,7 +35,7 @@ const Footer = () => {
                 <a className="link link-hover text-main">Privacy policy</a>
                 <a className="link link-hover text-main">Cookie policy</a>
             </nav>
-            <form>
+            <form className="hidden md:block">
                 <header className="footer-title text-highlight_color">Newsletter</header>
                 <fieldset className="form-control w-full ">
                     <label className="label">
