98a6a73e7524a8c195539cb4a3ca778b421a6903
joykbiswas <joykumarbiswas100@gmail.com>
2024-01-19 15:12:17 +0600

    fixed bug template section

--- components/Home/Feature/FeatureCard.js
+++ components/Home/Feature/FeatureCard.js
@@ -1,15 +1,15 @@
 import FeatureLottieIcon from "./FeatureLottieIcon";
 
-const FeatureCard = ({ icon, title,description}) => {
-    return (
-        <>
-            <div className='w-96 rounded-xl p-4  shadow-4xl text-white bg-gray-800 dark:bg-gray-900 dark:text-gray-50 '>
-                     <FeatureLottieIcon icon={icon}/>
-                       <h2 className='font-bold text-center  mb-4'>{title}</h2>
-                      <p className='text-center'>{description}</p>  
-                      </div>
-        </>
-    );
+const FeatureCard = ({ icon, title, description }) => {
+  return (
+    <>
+      <div className="m-2 p-2 rounded-xl   shadow-4xl text-white bg-gray-800 dark:bg-gray-900 dark:text-gray-50  shadow-inner  shadow-sub_color hover:shadow-lg hover:shadow-sub_color">
+        <FeatureLottieIcon icon={icon} />
+        <h2 className="font-bold text-center  mb-4">{title}</h2>
+        <p className="text-center">{description}</p>
+      </div>
+    </>
+  );
 };
 
 export default FeatureCard;
--- components/Home/Template/Template.js
+++ components/Home/Template/Template.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 import img from "@/assets/template-img.jpg";
 const Template = () => {
   return (
-    <div className=" py-16 mb-20 bg-neutral-100">
+    <div className=" py-16 mb-20 bg-[#fff7ed]">
       <div className="md:flex gap-16 max-w-[2520px] mx-auto xl:px-20 md:pr-10 sm:px-2 px-4">
         <div className="flex-1 mt-10 space-y-9 pl-6">
-          <h1 className="text-3xl md:text-5xl font-semibold">The Right <span className="text-highlight_color">Resume</span> & <span className="text-highlight_color">CV</span> Maker for the Job</h1>
-          <p className="text-2xl md:text-3xl text-neutral-600">
+          <h1 className="text-3xl md:text-4xl font-semibold">The Right <span className="text-highlight_color">Resume</span> & <span className="text-highlight_color">CV</span> Maker for the Job</h1>
+          <p className="text-xl md:text-2xl text-neutral-600">
             Tech, Engineering, Management or Marketing - CV generator with the
             right resume templates to highlight your career journey.
           </p>
-          <button className="text-2xl border px-8 hover:px-9 py-4 rounded-lg shadow-highlight_color shadow-lg hover:shadow-sub_color hover:shadow-xl bg-sub_color text-neutral-50 hover:bg-highlight_color hover:text-main  font-semibold ">Explore all template</button>
+          <button className="text-base md:text-xl border px-4 hover:px-5 md:px-8  md:hover:px-9 py-3 md:py-4 rounded-full shadow-highlight_color shadow-lg hover:shadow-sub_color hover:shadow-xl bg-sub_color text-neutral-50 hover:bg-highlight_color hover:text-main  font-semibold ">Explore all template</button>
 
         </div>
         <div className="flex-1 pt-10">
