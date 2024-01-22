e03b8943d5d63a5d901c1743482d12a1906ac5e9
Firoz Ahammad <firozahamed0@gmail.com>
2024-01-23 00:07:19 +0600

    Add Axios and tanstack Query

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,34 @@
+'use client'
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Shared/Navbar";
 import Footer from "@/components/Shared/Footer";
 import AuthProvider from "@/Providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const inter = Inter({ subsets: ["latin"] });
+const queryClient = new QueryClient()
 
-export const metadata = {
-  title: "ResuMagnet",
-  description: "A resume builder that will help you land your dream job.",
-};
+// export const metadata = {
+//   title: "ResuMagnet",
+//   description: "A resume builder that will help you land your dream job.",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html data-theme="light" lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className}>
+        <>
+      <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <Navbar></Navbar>
           {children}
           <Footer></Footer>
         </AuthProvider>
+      </QueryClientProvider>
+        </>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Banner from '@/components/Home/Banner/Banner'
 import Template from '@/components/Home/Template/Template'
 import Reviews from '@/components/Home/Review/Reviews'
 
-
+export const metadata = {
+    title: 'Home - ResuMagnet',
+    description: 'A resume builder that will help you land your dream job.',
+  };
 
 export default function Home() {
   return (
