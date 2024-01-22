6176a4874d149c168e089d8768279864cd307e88
Firoz Ahammad <firozahamed0@gmail.com>
2024-01-22 19:36:48 +0600

    Add Logo animation to loading screen

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,10 @@
+import Logo from "@/components/LogoAnimation/Logo";
 
 const loading = () => {
   return (
-    <div>loading</div>
+    <div className="h-screen w-full flex items-center justify-center justify-items-center mx-auto ">
+      <Logo/>
+    </div>
   )
 }
 
