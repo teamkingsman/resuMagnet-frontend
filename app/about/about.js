4685807b1b0b1fd19eefcd3fa0139670a15e14d4
ayon121 <borshonsaha1234@gmail.com>
2024-01-23 23:19:22 +0600

    basic about setup

---
+++ app/about/about.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "About Us",
+    description: "This is a About Us page",
+};
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ components/About/AboutUs.js
@@ -0,0 +1,11 @@
+
+
+const AboutUs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AboutUs;
