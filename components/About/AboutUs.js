aea91874354faf8641b4612b5f515e1fdf2f4f0b
ayon121 <borshonsaha1234@gmail.com>
2024-01-25 01:58:45 +0600

    about us completed

--- components/About/AboutUs.js
+++ components/About/AboutUs.js
@@ -4,15 +4,16 @@ import { FaCcAmazonPay } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { FaMicrosoft } from "react-icons/fa";
 import { FaEbay } from "react-icons/fa";
+import { SiIbm } from "react-icons/si";
 
 const AboutUs = () => {
     return (
         <div className="max-w-6xl mx-auto px-4 mt-4 mb-4" >
-            <h1 className="text-2xl md:text-3xl font-extrabold py-1">Welcome to Resumagnet</h1>
-            <p className="text-lg md:text-xl mb-4">Crafting Your Professional Journey Since January 2024</p>
-            <hr />
+            <h1 className="text-2xl md:text-3xl font-extrabold py-1 text-sub_color">Welcome to Resumagnet</h1>
+            <p className="text-lg md:text-xl mb-4 text-main">Crafting Your Professional Journey Since January 2024</p>
+            <hr className="text-highlight_color"/>
             <div className="team-section mt-3">
-                <h2 className="text-xl md:text-2xl font-semibold">Meet the Kingsman Team</h2>
+                <h2 className="text-xl md:text-2xl font-semibold text-sub_color">Meet the Kingsman Team</h2>
                 <p className="py-2">
                     We are a dedicated group of professionals passionate about helping individuals create
                     compelling CVs, resumes, and cover letters that stand out in the competitive job market.
@@ -23,7 +24,7 @@ const AboutUs = () => {
             </div>
 
             <div className="mission-section">
-                <h2 className="text-xl md:text-2xl font-semibold">Our Mission</h2>
+                <h2 className="text-xl md:text-2xl font-semibold text-sub_color">Our Mission</h2>
                 <p className="py-2">
                     At Kingsman, our mission is to empower you on your professional journey. We understand
                     the importance of a well-crafted CV and cover letter in opening doors to new
@@ -32,20 +33,20 @@ const AboutUs = () => {
                 </p>
             </div>
             <div className="mission-section">
-                <h2 className="text-xl md:text-2xl font-semibold">Our Team Members</h2>
+                <h2 className="text-xl md:text-2xl font-semibold text-sub_color">Our Team Members</h2>
                 <p className="py-2">
                     Meet the brilliant minds behind Kingsman Team - a dynamic ensemble of innovators, creatives, and professionals, each weaving their unique skills into the tapestry of our success.
-                    <li className="px-4 py-2">Firoz Ahammad <span>(MERN Stack Developer)</span></li>
-                    <li className="px-4 py-2">Md Sakib Hossion <span>(MERN Stack Developer)</span></li>
-                    <li className="px-4 py-2">Syed Abdullah Herra <span>(MERN Stack Developer)</span></li>
-                    <li className="px-4 py-2">Moin Uddin <span>(MERN Stack Developer)</span></li>
-                    <li className="px-4 py-2">Barshon Saha Ayon <span>(MERN Stack Developer)</span></li>
-                    <li className="px-4 py-2">Joy Biswas <span>(MERN Stack Developer)</span></li>
+                    <li className="px-4 py-2 text-sm md:text-lg lg:text-xl">Firoz Ahammad <sub>(MERN Stack Developer)</sub></li>
+                    <li className="px-4 py-2 text-sm md:text-lg lg:text-xl">Md Sakib Hossion <sub>(MERN Stack Developer)</sub></li>
+                    <li className="px-4 py-2 text-sm md:text-lg lg:text-xl">Syed Abdullah Herra <sub>(MERN Stack Developer)</sub></li>
+                    <li className="px-4 py-2 text-sm md:text-lg lg:text-xl">Moin Uddin <sub>(MERN Stack Developer)</sub></li>
+                    <li className="px-4 py-2 text-sm md:text-lg lg:text-xl">Barshon Saha Ayon <sub>(MERN Stack Developer)</sub></li>
+                    <li className="px-4 py-2 text-sm md:text-lg lg:text-xl">Joy Biswas <sub>(MERN Stack Developer)</sub></li>
                 </p>
             </div>
 
             <div className="timeline-section">
-                <h2 className="text-xl md:text-2xl mt-3 font-semibold">We’re Building the World’s Largest and Best Career Site to Help You Find the Job You Want.</h2>
+                <h2 className="text-xl md:text-2xl mt-3 font-semibold text-sub_color">We’re Building the World’s Largest and Best Career Site to Help You Find the Job You Want.</h2>
                 <p className="py-2">
                     Since our inception in January 2024, we have been committed to providing top-notch
                     services. We continuously evolve our platform, incorporating the latest trends in resume
@@ -54,7 +55,7 @@ const AboutUs = () => {
             </div>
 
             <div className="vision-section">
-                <h2 className="text-xl md:text-2xl mt-3 font-semibold">Our Vision</h2>
+                <h2 className="text-xl md:text-2xl mt-3 font-semibold text-sub_color">Our Vision</h2>
                 <p className="py-2">
                     We envision a future where every professional has the opportunity to present their
                     achievements in the best light possible. Through innovation and user-centric design, we
@@ -63,15 +64,16 @@ const AboutUs = () => {
                 </p>
             </div>
             <div className="vision-section">
-                <h2 className="text-xl md:text-2xl mt-3 font-semibold">Get Hired Faster: Companies Prefer Candidates with Resumes from Resumegnet</h2>
-                <div className="max-w-4xl px-2">
+                <h2 className="text-xl md:text-2xl mt-3 font-semibold text-sub_color">Get Hired Faster: Companies Prefer Candidates with Resumes from Resumegnet</h2>
+                <div className="max-w-5xl px-2">
                     <marquee behavior="" direction="">
-                        <p className="flex gap-5 px-5 ">
+                        <p className="flex gap-5 px-5 py-2">
                             <FaAmazon className="text-2xl md:text-5xl" />
                             <FaCcAmazonPay  className="text-2xl md:text-5xl"/>
                             <FaGoogle className="text-2xl md:text-5xl"/>
                             <FaMicrosoft className="text-2xl md:text-5xl"/>
                             <FaEbay className="text-2xl md:text-5xl"/>
+                            <SiIbm className="text-2xl md:text-5xl" />
                             </p>
                         <p></p>
                     </marquee>
@@ -79,7 +81,7 @@ const AboutUs = () => {
             </div>
 
             <div className="contact-section">
-                <h2 className="text-xl md:text-2xl mt-3 font-semibold">Get in Touch</h2>
+                <h2 className="text-xl md:text-2xl mt-3 font-semibold text-sub_color">Get in Touch</h2>
                 <p className="py-2">
                     Have questions or suggestions? We would love to hear from you! Contact the Kingsman Team
                     at <a href="team.kingsman2024@gmail.com" className="font-bold">team.kingsman2024@gmail.com</a>.
