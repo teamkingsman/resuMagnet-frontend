52fb936d484062bb1d10b4fb48d50c07e57b67da
Firoz Ahammad <firozahamed0@gmail.com>
2024-01-26 19:31:36 +0600

    Add saveUser function and update handleGoogleSignIn

--- components/Auth/SocialSignIn/SocialSignIn.js
+++ components/Auth/SocialSignIn/SocialSignIn.js
@@ -4,6 +4,7 @@ import { FaFacebookSquare, FaGoogle } from "react-icons/fa";
 import { useRouter } from 'next/navigation'
 import { toast } from "sonner";
 import useAuth from "@/hooks/useAuth";
+import { saveUser } from "@/lib/auth";
 // import usePreviousRoute from "@/hooks/usePreviousRoute";
 
 
@@ -11,32 +12,38 @@ function SocialSignIn() {
   const { googleSignIn, user } = useAuth();
   const router = useRouter();
   // const previousRoute = usePreviousRoute();
-  const handleGoogleSignIn = () => {
+  const handleGoogleSignIn = async () => {
     const toastId = toast.loading('Loading...');
 
     googleSignIn()
-        .then(result => {
-            console.log(result.user);
-            toast.success('Sign In successful', (toastId));
-            router.back()
-            // if (previousRoute) {
-            //   router.replace(previousRoute);
-            // } else {
-            //   router.push('/'); // Default redirect if no previous route
-            // }
-        })
-        .catch(error => {
-          console.error('Sign-in failed:', error);
-          toast.error(`Sign-in failed: ${error}`, (toastId));
-        })
-}
-
-useEffect(() => {
-  // Optionally, you can automatically redirect if the user is already signed in
-  if (user) {
-    router.replace('/');
+      .then( async(result) => {
+        console.log(result.user);
+        const currentUser = {
+          email: result.user.email,
+          displayName: result.user.displayName,
+          photoURL: result.user.photoURL,
+        };
+        await saveUser(currentUser)
+        toast.success('Sign In successful', (toastId));
+        // router.back()
+        // if (previousRoute) {
+        //   router.replace(previousRoute);
+        // } else {
+        //   router.push('/'); // Default redirect if no previous route
+        // }
+      })
+      .catch(error => {
+        console.error('Sign-in failed:', error);
+        toast.error(`Sign-in failed: ${error}`, (toastId));
+      })
   }
-}, [user, router]);
+
+  // useEffect(() => {
+  //   // Optionally, you can automatically redirect if the user is already signed in
+  //   if (user) {
+  //     router.replace('/');
+  //   }
+  // }, [user, router]);
 
   return (
     <div className="flex flex-col items-center  space-y-4 px-8">
