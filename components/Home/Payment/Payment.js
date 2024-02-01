7cc253952cb38b22d755e2f5dec52c0414a850b4
ayon121 <borshonsaha1234@gmail.com>
2024-02-01 11:24:54 +0600

    payment card design completed

--- components/Home/Payment/Payment.js
+++ components/Home/Payment/Payment.js
@@ -4,10 +4,11 @@ import PaymentCard from './PaymentCard';
 const Payment = () => {
     return (
         <div>
+            <h1 className='text-2xl md:text-4xl text-center px-6 font-bold mt-4'>Trusted Tool For Everybody</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-3 gap-2'>
-            <PaymentCard  price={'12'} title={'Students'} subtitle={'Basic'}></PaymentCard>
-            <PaymentCard price={'40'} title={'Job Skeers'} subtitle={'Advance'}></PaymentCard>
-            <PaymentCard price={'80'} title={'Professionals'} subtitle={'Most Popular'}></PaymentCard>
+            <PaymentCard  price={'12'} title={'Students'} subtitle={'Basic'} feature1={'Unlimited resumes & cover letters'} feature2={'Image Croping'} feature3={'Interviews Practice Questions'}></PaymentCard>
+            <PaymentCard price={'40'} title={'Job Hunters'} subtitle={'Advance'} feature2={'All premium templates & colors'} feature1={'Unlimited PDF downloads'} feature3={'Job Searching'}></PaymentCard>
+            <PaymentCard price={'80'} title={'Professionals'} subtitle={'Most Popular'} feature1={'ATS Verification'} feature2={'All premium templates & colors'} feature3={'Unlimited resumes , cover letters & CV'}></PaymentCard>
             </div>
         </div>
     );
