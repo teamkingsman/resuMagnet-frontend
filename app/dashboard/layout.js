7c23df59f35b254a6cbf381646d422f3b9833196
Firoz Ahammad <firozahamed0@gmail.com>
2024-02-02 20:34:09 +0600

    Refactor dashboard layout grid column spans

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -7,10 +7,10 @@ const DashboardLayout =  ({ children }) => {
   return (
     <PrivateRoute>
     <div className="grid grid-cols-12 gap-4 w-full ">
-      <div className="col-span-2 ">
+      <div className="lg:col-span-2 ">
         <Sidebar></Sidebar>
       </div>
-      <div className=" col-span-10 xl:px-10 md:pr-5 sm:px-2 px-2 pt-14">{children}</div>
+      <div className=" col-span-10 xl:px-10 md:pr-5 sm:px-2 px-2">{children}</div>
     </div>
     </PrivateRoute>
   );
