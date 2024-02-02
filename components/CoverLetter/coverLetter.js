7b191c88bafde3582ac0af0090667b429e8ff094
Moinuddin-hub <moincse022@gmail.com>
2024-02-02 20:18:36 +0600

    coverLetter done

--- components/CoverLetter/coverLetter.js
+++ components/CoverLetter/coverLetter.js
@@ -1,44 +1,43 @@
 
-import coverLetter from '../../assets/coverLetter.json'
-const CoverLetter = () => {
+// import coverLetter from '../../assets/coverLetter.json'
+const CoverLetter = ({coverLetter}) => {
     console.log(coverLetter);   
-  
+    const { designation, email, fname, lname, letterBody, phone } = coverLetter;
     return (
         <>
              <div className="shadow-2xl  bg-slate-200 font-serif w-[400px]  lg:w-[800px] h-[1200px] mx-auto mt-4">
-            {
-                coverLetter.map((coverLetter,index) => (
-                  <div key={index}>
+            
+         
+                  <div>
                       <div className="">
-                 <h2 className="text-center text-2xl font-bold mt-4">{coverLetter.fname}{coverLetter.lname}</h2>
+                 <h2 className="text-center text-2xl font-bold mt-4">{fname}{lname}</h2>
               </div>
                    {/* horizontal */}
                    <div className="space-y-4 w-[300px] lg:w-[750px] mx-auto bg-cyan-600">
                 <hr className="mt-4 h-2  "/>
                 </div>
                 <div className="flex text-sm lg:text-lg justify-center mt-2 gap-2 lg:gap-4">
-                    {/* <h2>{coverLetter?.sender?.address}</h2> */}
-                    <p>{coverLetter?.phone}</p>
-                    <p>{coverLetter?.email}</p>
+                 
+                    <p>{phone}</p>
+                    <p>{email}</p>
                 </div>
 
                 <div className="mt-8 ml-8 mr-4 text-sm lg:text-xl">
-                    {/* <h2 className="">{coverLetter.date}</h2> */}
-                    <h2 className="">{coverLetter.designation}</h2>
-                    <p>{coverLetter.phone}</p>
-                    <p>{coverLetter.email}</p>
-                    {/* <h2 className="">{coverLetter.recipient.company}</h2>
-                     <p className="mt-4">{coverLetter.recipient.name}</p> */}
-                     <div className=" justify-center">
-                        <p className='display-linebreak'>{coverLetter?.letterBody}</p>
-                         
                  
+                    <h2 className="">{designation}</h2>
+                    <p>{phone}</p>
+                    <p>{email}</p>
+                   
+                     <div className=" justify-center">
+                        <p className='display-linebreak'>{letterBody}</p>
+                         
+                     
                      </div>
                 </div>
 
                     </div>
-                    ))
-            }
+               
+            
              </div>
         </>
     );
