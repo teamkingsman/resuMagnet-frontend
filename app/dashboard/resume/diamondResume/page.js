f767fe8d933fc16c0f194b7b11980605ac50eab3
heeraraiyan <heeraraiyan07@yahoo.com>
2024-02-02 20:50:29 +0600

    cv,cover,resume reate with form

--- app/dashboard/resume/diamondResume/page.js
+++ app/dashboard/resume/diamondResume/page.js
@@ -1,13 +1,33 @@
+"use client"
+import useAuth from "@/hooks/useAuth";
+
+import { useEffect, useState } from "react";
+import { resumeFromGet } from "@/lib/BuilderAPI";
+
 import ResumeDiamond from "./ResumeDiamond";
 
 
-const page = () => {
+const DiamondResume = () => {
+
+    const {user} = useAuth();
+    const [resume, setResume] = useState({})
+
+    
+    useEffect(()=>{
+        resumeFromGet(user.email)
+        .then (res => setResume(res))
+    }, [user])
+    resumeFromGet(resume)
+
+
     return (
         <div className="bg-[#AEB6BF] py-20">
-            <ResumeDiamond></ResumeDiamond>
+            <ResumeDiamond resume={resume}></ResumeDiamond>
             
         </div>
     );
 };
 
-export default page;
+export default DiamondResume;
+
+
