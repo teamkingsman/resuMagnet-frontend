9e10f6d3d629dd40f3df7f60e24d6ee5136a12c0
joykbiswas <joykumarbiswas100@gmail.com>
2024-02-04 22:23:04 +0600

    resume template create

--- app/dashboard/resume/joytemplate/Resume.js
+++ app/dashboard/resume/joytemplate/Resume.js
@@ -1,185 +1,182 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
-import { FaLinkedinIn } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
-
-const Resume = (resumeData) => {
-  // const {
-  //   professionalExperience,
-  //   education,
-  //   skills,
-  //   languages,
-  //   certificates,
-  //   personalSkills,
-  // } = resumeData;
-  const {basicInfo, education, employment, languages, projects, skills} = resumeData
-  console.log(resumeData);
+import { IoIosPeople } from "react-icons/io";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+import { GiPostOffice } from "react-icons/gi";
+import { IoEarth } from "react-icons/io5";
+import Image from "next/image";
+const Resume = ({ resume }) => {
+  const { basicInfo, education, employment, languages, skills, projects } = resume;
   return (
-    <div className="max-w-screen-lg mx-auto border-2 p-5 font-poppins">
+    <div className="max-w-screen-lg mx-auto border-2 p-5 my-4 font-poppins">
       <div className="w-full">
         <div className="flex  items-end gap-2">
           <h1 className="text-2xl md:text-3xl font-semibold">
-            {basicInfo?.fname}{basicInfo?.lname}
+            {basicInfo?.fname} {basicInfo?.lname}
           </h1>
           <h2 className="font-lobster text-lg md:text-xl">
             {basicInfo?.designation}
           </h2>
         </div>
+        {/* details */}
+        <div className="flex">
+          <div className="grid grid-cols-2 w-4/5">
+            <h2 className="flex items-center gap-2">
+              <MdEmail />
+              {basicInfo?.email}
+            </h2>
+            <h2 className="flex items-center gap-2">
+              <MdOutlinePermContactCalendar />
+              {basicInfo?.dob}
+            </h2>
+            <h2 className="flex items-center gap-2 ">
+              <span>
+                <IoEarth />
+              </span>
+              {basicInfo?.city}, {basicInfo?.country}
+            </h2>
+            <h2 className="flex items-center gap-2">
+              <FaPhone />
+              {basicInfo?.phone}
+            </h2>
+            <h2 className="flex items-center gap-2">
+              <span>
+                <FaLocationDot />
+              </span>
+              {basicInfo?.street},
+            </h2>
 
-        <div className="grid grid-cols-2">
-          <h2 className="flex items-center gap-2">
-            <span>
-              <FaLocationDot />
-            </span>
-            {basicInfo?.country},{basicInfo?.city}
-          </h2>
-          <h2 className="flex items-center gap-2">
-            <MdEmail />
-            {basicInfo?.email}
-          </h2>
-          <h2 className="flex items-center gap-2">
-            <FaPhone />
-            {basicInfo?.phone}
-          </h2>
-          <h2 className="flex items-center gap-2">
-            <FaLinkedinIn />
-            {basicInfo?.linkedin}
-          </h2>
-          <h2 className="flex items-center gap-2">
-            <FaGithub />
-            {basicInfo?.github}
-          </h2>
+            <h2 className="flex items-center gap-2">
+              <IoIosPeople />
+              {basicInfo?.nationality}
+            </h2>
+            <h2 className="flex items-center gap-2 ">
+              <span>
+                <GiPostOffice />
+              </span>
+              {basicInfo?.postal}
+            </h2>
+          </div>
+          <div className="">
+            <Image
+              src={basicInfo?.photoURL}
+              alt="User Image"
+              className="rounded-full m-5"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
       </div>
 
       {/* profile */}
 
-      {/* <div className="pt-5">
-        <h1 className="text-xl font-semibold">Profile</h1>
-        <hr className="border-2 border-black"></hr>
-        <p>{basicInfo?.designation}</p>
-      </div> */}
-      {/* employment*/} 
+      <div className="pt-5">
+        <h1 className="text-xl font-semibold">About</h1>
+        <hr className="border border-black"></hr>
+        <p className="pt-2">{basicInfo?.about}</p>
+      </div>
+      {/* employment */}
       <div className="pt-5">
         <h1 className="text-xl font-semibold">Professional Experience</h1>
-        <hr className="border-2 border-black"></hr>
-        {employment?.map((employment, index) => (
+        <hr className="border border-black"></hr>
+        {employment?.map((emp, index) => (
           <div key={index} className="pt-2 grid grid-cols-12 gap-2">
             <div className="col-span-3">
-              <h1>
-                {employment?.startDate} - {employment?.endDate}
-              </h1>
-              <h2>{employment?.location}</h2>
+            <h2>
+                  {new Date(edu?.startDate).toISOString().slice(0, 10)} -
+                  {edu?.ongoing
+                    ? "Present"
+                    : new Date(edu?.endDate).toISOString().slice(0, 10)}
+                </h2>
+              
             </div>
             <div className="col-span-9">
-              <h2 className="text-xl font-medium">{employment?.jobTitle}</h2>
-              <h3 className="font-lobster">{employment?.employer}</h3>
+              <h2 className="text-xl font-medium">{emp?.employer}</h2>
+              <h3 className="font-lobster">{emp?.jobTitle}</h3>
               <ul className="list-disc pl-3">
-                {employment?.responsibilities.map((jobDescription, idx) => (
-                  <li key={idx}>{jobDescription}</li>
-                ))}
+                <li>{emp?.jobDescription}</li>
               </ul>
             </div>
           </div>
         ))}
       </div>
       {/* Education */}
+
       <div className="pt-5 ">
         <h1 className="text-xl font-semibold">Education</h1>
-        <hr className="border-2 border-black"></hr>
+        <hr className="border border-black"></hr>
         <div className="grid grid-cols-2 pt-4 gap-3">
           {education?.map((edu, index) => (
-            <div key={index} className="flex md:gap-5 gap-3 ">
+            <div key={index} className="flex md:gap-5 gap-3">
               <div>
                 <h2>
-                  {edu?.startDate} - {edu?.endDate}
+                  {new Date(edu?.startDate).toISOString().slice(0, 10)} -
+                  {edu?.ongoing
+                    ? "Present"
+                    : new Date(edu?.endDate).toISOString().slice(0, 10)}
                 </h2>
-                <h3>{edu?.location}</h3>
+                {/* <h3>{edu.location}</h3> */}
               </div>
               <div>
                 <h3 className="font-semibold">{edu?.degree}</h3>
-                <h2>{edu?.institution}</h2>
+                <h2>{edu?.institute}</h2>
+                <ul className="list-disc  pl-3">
+                  <li>{edu?.jobDescription}</li>
+                </ul>
               </div>
             </div>
           ))}
         </div>
       </div>
+      {/* projects */}
+      <div className="pt-5">
+        <h1 className="text-2xl font-semibold">Project</h1>
+        <hr className="border border-black"></hr>
+        {projects?.map((project, index) => (
+          <div key={index} className="text-xl pt-3">
+            <h2 className="font-semibold">
+              {project.title}
+              <span className="text-base font-medium">{project?.type}</span>
+            </h2>
+            <h3>{project?.liveLink}</h3>
+            <h3>{project?.githubLink}</h3>
+            <ul className="list-disc pl-4">
+              <li>{project?.description}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
       {/* SKILLs */}
       <div className="pt-5">
-              <h1 className="text-2xl font-semibold">SKILLS</h1>
-              <hr className="border"></hr>
-              <div className="text-xl pt-3">
-                <ul className="list-disc pl-4">
-                  {skills?.map((skill, index) => (
-                    <li key={index}>
-                      <span className="font-semibold">{skill?.level} :</span>
-                      <span>{skill?.skill}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-        {/* language */}
+        <h1 className="text-xl font-semibold">Skills</h1>
+        <hr className="border border-black"></hr>
+        <div className="grid grid-cols-3  gap-4">
+          <ul className="list-disc flex pt-3 pl-4 md:gap-12 gap-4">
+            {skills?.map((lang, index) => (
+              <li key={index}>
+                <span className="text-lg font-semibold">{lang?.level}</span>:{" "}
+                {lang?.skill}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/* Languages */}
       <div className="pt-5">
-              <h1 className="text-2xl font-semibold">SKILLS</h1>
-              <hr className="border"></hr>
-              <div className="text-xl pt-3">
-                <ul className="list-disc pl-4">
-                  {languages?.map((language, index) => (
-                    <li key={index}>
-                      <span className="font-semibold">{language?.proficiency} :</span>
-                      <span>{language?.language}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-              {/* Project */}
-            <div className="pt-5">
-            <h1 className="text-2xl font-semibold">Project</h1>
-            <hr className="border"></hr>
-            {projects?.map((project, index) => (
-              <div key={index} className="text-xl pt-3">
-                <h2 className="font-semibold">
-                  {project?.title}{" "}
-                  <span className="text-base font-medium">{project?.type}</span>
-                </h2>
-                <h3>{project?.liveLink}</h3>
-                <h3>{project?.githubLink}</h3>
-                <ul className="list-disc pl-4">
-                  <li>{project?.description}</li>
-                </ul>
-              </div>
-            ))}
-            
-            
-          </div>
-      {/* Certificate */}
-      {/* <div className="pt-5">
-        <h1 className="text-xl font-semibold">Certificate</h1>
-        <hr className="border-2 border-black"></hr>
+        <h1 className="text-xl font-semibold">Languages</h1>
+        <hr className="border border-black"></hr>
         <div>
           <ul className="list-disc flex pt-3 pl-4 md:gap-12 gap-4">
-            {certificates?.map((certificate, index) => (
-              <li key={index}>{certificate}</li>
+            {languages?.map((lang, index) => (
+              <li key={index}>
+                {lang?.language} ({lang?.proficiency})
+              </li>
             ))}
           </ul>
         </div>
-      </div> */}
-
-      {/* Person Skills */}
-      {/* <div className="pt-5">
-        <h1 className="text-xl font-semibold">Personal Skills</h1>
-        <hr className="border-2 border-black"></hr>
-        <div>
-          <ul className="list-disc flex pt-3 pl-4 md:gap-12 gap-4">
-            {personalSkills?.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
-        </div>
-      </div> */}
+      </div>
     </div>
   );
 };
--- app/dashboard/resume/joytemplate/page.js
+++ app/dashboard/resume/joytemplate/page.js
@@ -1,21 +1,24 @@
-// "use client";
-// import useAuth from "@/hooks/useAuth";
-// import Resume from "./Resume";
-// import { useEffect, useState } from "react";
-// import { resumeFromGet } from "@/lib/BuilderAPI";
+"use client";
+import useAuth from "@/hooks/useAuth";
+import Resume from "./Resume";
+import { useEffect, useState } from "react";
+import { resumeFromGet } from "@/lib/BuilderAPI";
 
-// const JoyResume = () => {
-//   const { user } = useAuth();
-//   const [resumeData, setResumeData] = useState({});
-//   console.log(resumeData);
-//   useEffect(() => {
-//     resumeFromGet(user.email).then((res) => setResumeData(res));
-//   }, [user]);
-//   return (
-//     <div>
-//       <Resume resumeData={resumeData}></Resume>
-//     </div>
-//   );
-// };
+const JoyResume = () => {
+    const {user} = useAuth();
+    const [resume, setResume] = useState({})
 
-// export default JoyResume;
+    console.log(resume);
+    useEffect(()=>{
+        resumeFromGet(user.email)
+        .then (res => setResume(res))
+    }, [user])
+    resumeFromGet(resume)
+  return (
+    <div>
+      <Resume resume={resume}></Resume>
+    </div>
+  );
+};
+
+export default JoyResume;
