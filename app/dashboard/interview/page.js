27bf878bc15d970a75779877b3019e7f9a677716
ayon121 <borshonsaha1234@gmail.com>
2024-02-05 23:44:37 +0600

    basic setup

---
+++ app/dashboard/interview/page.js
@@ -0,0 +1,11 @@
+import Interview from "@/components/InterviewQ&A/Interview";
+
+const page = () => {
+    return (
+        <div>
+            <Interview></Interview>
+        </div>
+    );
+};
+
+export default page;
---
+++ components/InterviewQ&A/Interview.js
@@ -0,0 +1,10 @@
+
+const Interview = () => {
+    return (
+        <div>
+            <h1>Search</h1>
+        </div>
+    );
+};
+
+export default Interview;
