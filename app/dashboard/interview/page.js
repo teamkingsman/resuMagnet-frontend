8975fd9d0302e9ed2c390373bc6c5df142743cee
ayon121 <borshonsaha1234@gmail.com>
2024-02-06 14:52:18 +0600

    frontend update

--- app/dashboard/interview/page.js
+++ app/dashboard/interview/page.js
@@ -1,8 +1,14 @@
+
 import Interview from "@/components/InterviewQ&A/Interview";
 
 const page = () => {
     return (
-        <div>
+        <div className="min-h-screen">
+            <div className="mt-4 px-3 w-full text-center">
+                <h1 className="text-3xl md:text-4xl mt-4 font-extrabold">Master the  Interview</h1>
+                <p className = "text-2xl md:text-3xl py-2 font-bold">Your Ultimate Guide to Landing Your Dream Job</p>
+            </div>
+            {/* input field */}
             <Interview></Interview>
         </div>
     );
