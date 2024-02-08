0a8b856eec222a30e9f2f05a86a73a1fc637ac6c
heeraraiyan <heeraraiyan07@yahoo.com>
2024-02-08 18:20:26 +0600

    home page modified color and margin

--- components/Home/Template/Template.js
+++ components/Home/Template/Template.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import img from "@/assets/template-img.jpg";
 const Template = () => {
   return (
-    <div className="py-6 my-6 bg-[#fff7ed]">
+    <div className="py-10  bg-gradient-to-r from-[#EAF2F8] to-[#FDF2E9] ...">
       <div className="flex flex-col md:flex-row justify-evenly items-center justify-items-center gap-16 max-w-[2520px] mx-auto xl:px-20 md:pr-10 sm:px-2 px-4">
         <div className="mt-10 space-y-9 pl-6">
           <h1 className="text-2xl font-semibold md:text-3xl lg:text-5xl">The Right <span className="text-highlight_color">Resume</span> & <span className="text-highlight_color">CV</span> Maker for the Job</h1>
