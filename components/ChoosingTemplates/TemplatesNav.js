936cea1ad631af4c6c3a6d645126988a6d3f5bda
joykbiswas <joykumarbiswas100@gmail.com>
2024-02-08 15:57:10 +0600

    daynamic create cv link

--- components/ChoosingTemplates/TemplatesNav.js
+++ components/ChoosingTemplates/TemplatesNav.js
@@ -3,16 +3,25 @@ import { IoCaretBack } from "react-icons/io5";
 
 import Link from "next/link";
 import useAuth from "@/hooks/useAuth";
-import { resumeFromGet } from "@/lib/BuilderAPI";
+import { cvFromGet, resumeFromGet } from "@/lib/BuilderAPI";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 const TemplatesNav = () => {
     const {user} = useAuth()
-  
+  const pathname= usePathname()
+  console.log(pathname);
   const [objectId, setObjectId] = useState("");
   useEffect(()=>{
-    resumeFromGet(user.email).then((res) => setObjectId(res._id))
+    if(pathname == '/dashboard/resume/preview' ){
+        resumeFromGet(user.email).then((res) => setObjectId(res._id))
     .catch((err) => console.log(err))
-  },[user.email])
+    }
+    if(pathname == '/dashboard/cv/preview'){
+        cvFromGet(user.email).then((res) => setObjectId(res._id))
+    .catch((err) => console.log(err))
+    }
+    
+  },[user.email,pathname])
   console.log(objectId);
     return (
         <div>
@@ -25,7 +34,8 @@ const TemplatesNav = () => {
                 <div className="flex-none">
                     <ul className="menu menu-vertical px-1">
                         <li>
-                            <div className="drawer drawer-end">
+                            {pathname !== "/dashboard/cover_letter/preview" && (
+                                <div className="drawer drawer-end">
                                 <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
                                 <div className="drawer-content">
                                     {/* Page content here */}
@@ -38,13 +48,23 @@ const TemplatesNav = () => {
                                     <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                                     <ul className="menu p-4 w-72 md:w-80 min-h-full bg-base-200 text-base-content">
                                         {/* Sidebar content here */}
-                                        <li>
+                                        {
+                                            pathname =='/dashboard/resume/preview' ?(
+                                                <li>
                                             <Link href={`/r/${objectId}`}>
                                             Copy Link</Link></li>
+                                            ) :(
+                                                <li>
+                                            <Link href={`/c/${objectId}`}>
+                                            Copy Link</Link></li>
+                                            )
+                                        }
+                                        
                                         <li className="btn btn-sm md:btn-md lg:btn-lg w-3/4 mx-auto bg-sub_color py-4 mt-4 "><span className="text-highlight_color"></span>Download PDF</li>
                                     </ul>
                                 </div>
                             </div>
+                            )}
                         </li>
                 
                     </ul>
