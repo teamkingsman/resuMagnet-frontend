d501b5efdb0135ac6f0ac31a6766c1debd82dd06
Moinuddin-hub <moincse022@gmail.com>
2024-02-25 09:10:23 +0600

    update quiz

---
+++ app/dashboard/quizApp/javascriptApp/page.js
@@ -0,0 +1,12 @@
+import Javascript from '@/components/QuizAplication/Javascript';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Javascript/>
+        </div>
+    );
+};
+
+export default page;
