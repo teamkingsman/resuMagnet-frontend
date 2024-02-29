21fb754d0052cbc0bf7b14c78d48833ee862219d
Hossain-sakib <mskib567@gmail.com>
2024-02-29 15:33:46 +0600

    completed job posting section

--- components/Home/JobPost/PostCard/PostCard.js
+++ components/Home/JobPost/PostCard/PostCard.js
@@ -4,10 +4,12 @@ import {
   AiOutlineArrowDown,
   AiOutlineArrowUp,
   AiOutlineComment,
+  AiOutlineSend,
 } from "react-icons/ai";
 import { useState } from "react";
 import axiosSecure from "@/lib";
 import useAuth from "@/hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
 
 function PostCard({ post, refetch }) {
   const { user } = useAuth();
@@ -36,6 +38,10 @@ function PostCard({ post, refetch }) {
   };
 
   const [showFullText, setShowFullText] = useState(false);
+  const [showCommentBox, setShowCommentBox] = useState(false);
+  const initialCommentsToShow = 3;
+  const [commentsToShow, setCommentsToShow] = useState(initialCommentsToShow);
+
   const toggleText = () => {
     setShowFullText((prev) => !prev);
   };
@@ -68,7 +74,6 @@ function PostCard({ post, refetch }) {
         });
       }
       const updatedPostData = await axiosSecure.get(`/posts/${_id}`);
-      console.log(updatedPostData);
       refetch();
       //   setPostData(updatedPostData.data);
     } catch (error) {
@@ -104,7 +109,6 @@ function PostCard({ post, refetch }) {
       }
       const updatedPostData = await axiosSecure.get(`/posts/${_id}`);
       refetch();
-      console.log(updatedPostData);
       //  setPostData(updatedPostData.data);
     } catch (error) {
       console.error("Error handling dislike:", error);
@@ -114,9 +118,53 @@ function PostCard({ post, refetch }) {
   const isLiked = likes?.includes(user?.email);
   const isDisliked = dislikes?.includes(user?.email);
 
+  //   comments
+  const handleCommentBoxToggle = () => {
+    setShowCommentBox((prev) => !prev);
+  };
+
+  const { data: comments = [] } = useQuery({
+    queryKey: ["comments", _id],
+    queryFn: async () => {
+      try {
+        const res = await axiosSecure.get(`/comment/${_id}`);
+        return res.data;
+      } catch (error) {
+        throw new Error(`Error fetching comments: ${error.message}`);
+      }
+    },
+  });
+  const handlePostComment = async (e) => {
+    e.preventDefault();
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString();
+    const form = e.target;
+    const newComment = form.comment.value;
+
+    const addComment = {
+      postId: _id,
+      commenterPhoto: user?.photoURL,
+      commenterName: user?.displayName,
+      commenterEmail: user?.email,
+      createdAt: formattedDate,
+      comment: newComment,
+    };
+
+    const addCommentRes = await axiosSecure.post("/comment", addComment);
+    refetch();
+  };
+
+  const handleSeeMoreComments = () => {
+    setCommentsToShow((prev) => prev + 3);
+  };
+
+  const handleSeeLessComments = () => {
+    setCommentsToShow(initialCommentsToShow);
+  };
+
   return (
-    <div className="border-2 border-main rounded-none bg-base-200 w-full">
-      <div className="p-4 space-y-4">
+    <div className="border-2 border-main rounded-none bg-base-200 w-full py-4">
+      <div className="px-4 space-y-2">
         <div className="flex items-center gap-2">
           <div className="flex items-center">
             <div className="avatar">
@@ -164,11 +212,11 @@ function PostCard({ post, refetch }) {
           />
         </div>
       </div>
-      <div className="bg-cyan-50 w-full flex justify-around gap-2 px-4 my-2">
-        <div className="w-1/3 flex items-center border border-cyan-400 justify-center overflow-hidden transition-all   hover:scale-105  hover:shadow-2xl rounded-lg">
+      <div className="w-full flex justify-around gap-2 px-4 my-2">
+        <div className="w-1/3 flex items-center border  border-main justify-center overflow-hidden transition-all   hover:scale-105  hover:shadow-2xl rounded-lg">
           <button
             onClick={handleLike}
-            className={`flex items-center  gap-1 text-xs font-bold text-cyan-600 p-1 ${
+            className={`flex items-center  gap-1 text-xs font-bold text-main p-1 ${
               isLiked ? "text-green-600" : ""
             }`}
           >
@@ -177,10 +225,10 @@ function PostCard({ post, refetch }) {
             {isLiked ? "Liked" : "Like"}
           </button>
         </div>
-        <div className="w-1/3 flex items-center border border-cyan-400 justify-center overflow-hidden transition-all   hover:scale-105  hover:shadow-2xl rounded-lg">
+        <div className="w-1/3 flex items-center border  border-main justify-center overflow-hidden transition-all   hover:scale-105  hover:shadow-2xl rounded-lg">
           <button
             onClick={handleDislike}
-            className={`flex items-center  gap-1 text-xs font-bold text-cyan-600 p-1 ${
+            className={`flex items-center  gap-1 text-xs font-bold text-main p-1 ${
               isDisliked ? "text-red-600" : ""
             }`}
           >
@@ -189,14 +237,87 @@ function PostCard({ post, refetch }) {
             {isDisliked ? "Disliked" : "Dislike"}
           </button>
         </div>
-        <div className="w-1/3 flex items-center border border-cyan-400 justify-center overflow-hidden transition-all   hover:scale-105  hover:shadow-2xl rounded-lg">
-          <button className="flex items-center  gap-1 text-xs font-bold text-cyan-600 p-1">
-            {/* {comments.length} */}
+        <div className="w-1/3 flex items-center border  border-main justify-center overflow-hidden transition-all   hover:scale-105  hover:shadow-2xl rounded-lg">
+          <button
+            onClick={handleCommentBoxToggle}
+            className="flex items-center  gap-1 text-xs font-bold text-main p-1"
+          >
+            {comments.length}
             <AiOutlineComment className="text-blue-600 text-xs"></AiOutlineComment>{" "}
-            Comment
+            Comments
           </button>
         </div>
       </div>
+      {showCommentBox && (
+        <div className="px-4 space-y-2">
+          {comments.slice(0, commentsToShow).map((comment) => (
+            <div key={comment._id} className="flex items-start flex-col  gap-2">
+              <div className="flex items-center gap-2">
+                <div className="avatar">
+                  <div className="w-8 h-8 rounded-full overflow-hidden transition-all hover:scale-105  hover:shadow-2xl ">
+                    <Image
+                      src={comment.commenterPhoto}
+                      alt=" "
+                      width={40}
+                      height={40}
+                      className="w-full object-cover rounded-none border border-main"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col items-start">
+                  <h1 className="text-main font-semibold">
+                    {comment.commenterName}
+                  </h1>
+                  <h1 className="text-sm font-medium text-main">
+                    {formatDate(comment.createdAt)}
+                  </h1>
+                </div>
+              </div>
+              <p className="text-sm ">{comment.comment}</p>
+              <div className="divider"></div>
+            </div>
+          ))}
+          {comments.length > commentsToShow && (
+            <div className="flex justify-center mt-2">
+              <button
+                onClick={handleSeeMoreComments}
+                className="text-base text-main font-normal cursor-pointer"
+              >
+                See More
+              </button>
+            </div>
+          )}
+          {commentsToShow > initialCommentsToShow && (
+            <div className="flex justify-center mt-2">
+              <button
+                onClick={handleSeeLessComments}
+                className="text-base text-main font-normal cursor-pointer"
+              >
+                See Less
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+      {showCommentBox && (
+        <form
+          onSubmit={handlePostComment}
+          className="px-4 w-full flex items-center"
+        >
+          <div className="form-control w-full">
+            <textarea
+              type="text"
+              name="comment"
+              placeholder="Write a comment...."
+              className="input-bordered textarea  border-main"
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="-ml-8">
+            <AiOutlineSend className="text-main text-2xl overflow-hidden transition-all hover:scale-105  hover:shadow-2xl"></AiOutlineSend>
+          </button>
+        </form>
+      )}
     </div>
   );
 }
