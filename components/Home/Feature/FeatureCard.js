444778378e8ff1cc205a1d9e56db3be487b22502
ayon121 <borshonsaha1234@gmail.com>
2024-03-04 03:14:51 -0800

    home page updated

--- components/Home/Feature/FeatureCard.js
+++ components/Home/Feature/FeatureCard.js
@@ -6,7 +6,7 @@ const FeatureCard = ({ icon, title, description }) => {
       <div className="m-2 p-2 rounded-xl   shadow-4xl text-white bg-gray-800 dark:bg-gray-900 dark:text-gray-50 bg-whitecolor  shadow-inner  shadow-main hover:shadow-lg hover:shadow-sub_color">
         <FeatureLottieIcon icon={icon} />
         <h2 className="font-bold text-center  mb-4">{title}</h2>
-        <p className="text-center">{description}</p>
+        <p className="text-center font-light">{description}</p>
       </div>
     </>
   );
--- components/Home/Feature/FeatureSection.js
+++ components/Home/Feature/FeatureSection.js
@@ -5,7 +5,7 @@ const FeatureSection = () => {
  
     return (
         <>
-        <div className='  h-full max-w-[2520px] mx-auto xl:px-20 md:pr-10 sm:px-2 pt-20 pb-20 bg-gradient-to-r from-[#EAF2F8] to-[#D6EAF8]'>
+        <div className='  h-full max-w-[2520px] mx-auto xl:px-20 md:pr-10 sm:px-2 pt-20 pb-20 bg-gradient-to-r from-[#EAF2F8] to-[#D6EAF8] mb-5 mt-3'>
             <h1 className='text-2xl font-semibold  lg:text-5xl  md:text-3xl text-center px-6 mx-auto text-main uppercase'>The Professional Resume Maker and CV Builder</h1>
             <h2 className='text-base md:text-xl my-6 text-center px-6 mx-auto  '>VisualCV is everything you need to create an online resume and make your next <br/> career move</h2>
             <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-3 pt-5 w-9/12 mx-auto'>
